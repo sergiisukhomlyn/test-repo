70031299e18776c7aee82f05b45b57fd9565e9d4
sergiisukhomlyn <qaname123@gmail.com>
2025-09-30 16:29:46 +0300

    main

---
+++ lesson3/src/arithmetic.js
@@ -0,0 +1,37 @@
+console.log('------------------');
+
+const a = 2.5;
+const b = 3.5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+console.log(a % b);
+
+console.log('------------------');
+
+const c = 2;
+const d = '4';
+
+console.log(c + d);
+console.log(c - d);
+console.log(c * d);
+console.log(c / d);
+console.log(c ** d);
+console.log(c % d);
+
+console.log('------------------');
+
+const e = 2;
+const f = 'string';
+
+console.log(e + f);
+console.log(e - f);
+console.log(e * f);
+console.log(e / f);
+console.log(e ** f);
+console.log(e % f);
+
+console.log('------------------');
---
+++ lesson3/src/switch.js
@@ -0,0 +1,15 @@
+const a = 3;
+switch (a) {
+    case 1: {
+        console.log('a === 1');
+        break;
+    }
+    case 2:
+        console.log('a === 2');
+        break;
+    case 3:
+        console.log('a === 3');
+        break;
+    default:
+        console.log('non of the conditions above');
+}
