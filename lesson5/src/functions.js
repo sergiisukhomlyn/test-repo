9833c112fa18ae6c4f4a4399187df7af72d363a4
sergiisukhomlyn <qaname123@gmail.com>
2025-10-08 16:53:49 +0300

    lesson5

--- lesson5/src/functions.js
+++ lesson5/src/functions.js
@@ -1,26 +1,67 @@
 function sumAny(a, b) {
     return a + b;
 }
+
 const result = sumAny(1, 2);
 const result2 = sumAny(2, '2');
 console.log(result, result2);
 
-function logArrayValues(arr) {
-    arr.forEach(element => {
+function logArrayValues(arr, type) {
+    if (!Array.isArray(arr)) {
+        return;
+    }
+
+    const resultingArray = type && typeof type === 'string' ? filterArrayByType(arr, type) : arr;
+
+    resultingArray.forEach(element => {
         console.log(element);
     });
 }
 
 const arr1 = [1, 2, 3];
 const arr2 = ['1', '2', '3'];
-console.log(logArrayValues(arr1));
-console.log(logArrayValues(arr2));
+
 
 export function filterArrayByType(arr, type) {
     return arr.filter(item => typeof item === type);
 }
 
-const anyArray = [1, '2', true, {num: 4}];
+export const anyArray = [1, '2', true, {num: 4}];
 
 console.log(filterArrayByType(anyArray, 'string'));
 console.log(filterArrayByType(anyArray, 'boolean'));
+
+
+logArrayValues(arr1);
+logArrayValues(arr2);
+logArrayValues(1);
+console.log('logArrayValues([...anyArray, ...[6, \'true\', false]]);');
+logArrayValues([...anyArray, ...[6, 'true', false]]);
+console.log('logArrayValues([...anyArray, ...[6, \'true\', false]], \'string\');');
+logArrayValues([...anyArray, ...[6, 'true', false]], 'string');
+console.log('logArrayValues([...anyArray, ...[6, \'true\', false]], true);');
+logArrayValues([...anyArray, ...[6, 'true', false]], true);
+
+
+class SomeClass {
+    name;
+    surName = 'SurName';
+
+    constructor() {
+        this.name = 'SomeClass';
+    }
+
+    logInfo() {
+        console.log(this.name, this.surName);
+        console.log(this);
+    }
+
+    logAnotherInfo = () => {
+        console.log(this);
+    };
+}
+
+const obj = new SomeClass();
+obj.logInfo();
+console.log('-----------');
+obj.logAnotherInfo();
